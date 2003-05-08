5c23eafe3089ee119bcf05f30740eefd6a873582
Aleksey Gurtovoy <agurtovoy@meta-comm.com>
2003-05-08 09:43:23 +0000

    restore from the CVS crash
    [SVN r18356]

--- include/boost/mpl/aux_/yes_no.hpp
+++ include/boost/mpl/aux_/yes_no.hpp
@@ -1,10 +1,12 @@
-//-----------------------------------------------------------------------------
-// boost mpl/aux_/yes_no.hpp header file
-// See http://www.boost.org for updates, documentation, and revision history.
-//-----------------------------------------------------------------------------
-//
-// Copyright (c) 2001-02
-// Peter Dimov, Aleksey Gurtovoy
+
+#ifndef BOOST_MPL_AUX_YES_NO_HPP_INCLUDED
+#define BOOST_MPL_AUX_YES_NO_HPP_INCLUDED
+
+// + file: boost/mpl/aux_/yes_no.hpp
+// + last modified: 05/may/03
+
+// Copyright (c) 2000-03
+// Aleksey Gurtovoy
 //
 // Permission to use, copy, modify, distribute and sell this software
 // and its documentation for any purpose is hereby granted without fee, 
@@ -13,19 +15,24 @@
 // supporting documentation. No representations are made about the 
 // suitability of this software for any purpose. It is provided "as is" 
 // without express or implied warranty.
+//
+// See http://www.boost.org/libs/mpl for documentation.
 
-#ifndef BOOST_MPL_AUX_YES_NO_HPP_INCLUDED
-#define BOOST_MPL_AUX_YES_NO_HPP_INCLUDED
-
-namespace boost {
-namespace mpl {
-namespace aux {
+namespace boost { namespace mpl { namespace aux {
 
 typedef char (&no_tag)[1];
 typedef char (&yes_tag)[2];
 
-} // namespace aux
-} // namespace mpl
-} // namespace boost 
+template< bool C_ > struct yes_no_tag
+{
+    typedef no_tag type;
+};
+
+template<> struct yes_no_tag<true>
+{
+    typedef yes_tag type;
+};
+
+}}}
 
 #endif // BOOST_MPL_AUX_YES_NO_HPP_INCLUDED
